92ce1ab898a1e0cb28c5b404ddd0ddb3e2ba57ee
Maciej Gołaszewski <jodator@jodator.net>
2017-12-06 13:17:25 +0100

    Define basic convertes for schemaItem.

--- src/schemastructureddataediting.js
+++ src/schemastructureddataediting.js
@@ -1,4 +1,8 @@
 import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
+import buildViewConverter from '../../ckeditor5-engine/src/conversion/buildviewconverter';
+import { eventNameToConsumableType } from '../../ckeditor5-engine/src/conversion/model-to-view-converters';
+
+const schemaOrgUri = 'http://schema.org/';
 
 /**
  * The editing feature of Structured Data plugin.
@@ -8,11 +12,84 @@ export default class SchemaStructuredDataEditing extends Plugin {
 	 * @inheritDoc
 	 */
 	init() {
-		const schema = this.editor.document.schema;
+		const editor = this.editor;
+		const data = editor.data;
+		const editing = editor.editing;
+		const schema = editor.document.schema;
 
 		schema.allow( { name: '$block', attributes: 'schemaItem', inside: '$root' } );
 
 		schema.allow( { name: '$inline', attributes: 'schemaItemProp', inside: '$block' } );
 		schema.allow( { name: '$inline', attributes: 'schemaItemProp', inside: '$clipboardHolder' } );
+
+		attributeToAttributesConverter(
+			[ data.modelToView, editing.modelToView ],
+			'schemaItem',
+			attribute => ( { itemtype: schemaOrgUri + attribute, itemscope: 'itemscope' } ),
+			() => [ 'itemtype', 'itemscope' ]
+		);
+
+		buildViewConverter().for( data.viewToModel )
+			.fromAttribute( 'itemtype' )
+			.consuming( { attribute: [ 'itemtype', 'itemscope' ] } )
+			.toAttribute( viewBlock => {
+				let attribute = viewBlock.getAttribute( 'itemtype' );
+
+				if ( !attribute ) {
+					return;
+				}
+
+				if ( attribute.startsWith( schemaOrgUri ) ) {
+					attribute = attribute.substr( schemaOrgUri.length );
+				}
+
+				return {
+					key: 'schemaItem',
+					value: attribute
+				};
+			} );
 	}
+}
+
+// Defines attribute to attributes converters.
+// @private
+function attributeToAttributesConverter( dispatchers, modelAttributeName, setAttributesFn, removeAttributesFn ) {
+	for ( const dispatcher of dispatchers ) {
+		dispatcher.on( `addAttribute:${ modelAttributeName }`, setAttributes( setAttributesFn ) );
+		dispatcher.on( `changeAttribute:${ modelAttributeName }`, setAttributes( setAttributesFn ) );
+		dispatcher.on( `removeAttribute:${ modelAttributeName }`, removeAttributes( removeAttributesFn ) );
+	}
+}
+
+// Dispatcher handler responsible for setting attributes to a view element.
+// @private
+function setAttributes( setAttributeFn ) {
+	return ( evt, data, consumable, conversionApi ) => {
+		if ( !consumable.consume( data.item, eventNameToConsumableType( evt.name ) ) ) {
+			return;
+		}
+
+		const attributes = setAttributeFn( data.attributeNewValue );
+
+		const viewElement = conversionApi.mapper.toViewElement( data.item );
+
+		for ( const key in attributes ) {
+			viewElement.setAttribute( key, attributes[ key ] );
+		}
+	};
+}
+
+// Dispatcher handler responsible for removing attributes from a view element.
+// @private
+function removeAttributes( removeAttributesFn ) {
+	return ( evt, data, consumable, conversionApi ) => {
+		if ( !consumable.consume( data.item, eventNameToConsumableType( evt.name ) ) ) {
+			return;
+		}
+
+		const attributes = removeAttributesFn();
+		const viewElement = conversionApi.mapper.toViewElement( data.item );
+
+		viewElement.removeAttribute( ...attributes );
+	};
 }
--- tests/schemastructureddataditing.js
+++ tests/schemastructureddataditing.js
@@ -2,6 +2,7 @@ import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
 
 import VirtualTestEditor from '@ckeditor/ckeditor5-core/tests/_utils/virtualtesteditor';
 import SchemaStructuredDataEditing from '../src/schemastructureddataediting';
+import { getData as getModelData, setData as setModelData } from '../../ckeditor5-engine/src/dev-utils/model';
 
 describe( 'SchemaStructuredDataEditing', () => {
 	let editor, doc;
@@ -27,5 +28,23 @@ describe( 'SchemaStructuredDataEditing', () => {
 		expect( doc.schema.check( { name: '$inline', attributes: 'schemaItemProp', inside: '$clipboardHolder' } ) ).to.be.true;
 
 		expect( doc.schema.check( { name: '$block', attributes: 'schemaItem', inside: '$root' } ) ).to.be.true;
+	} );
+
+	describe( 'data pipeline conversions', () => {
+		it( 'should convert block with defined item', () => {
+			editor.setData( '<p itemscope itemtype="http://schema.org/Article">foo</p>' );
+
+			expect( getModelData( doc ) ).to.equal( '<paragraph schemaItem="Article">[]foo</paragraph>' );
+
+			expect( editor.getData() ).to.equal( '<p itemtype="http://schema.org/Article" itemscope="itemscope">foo</p>' );
+		} );
+	} );
+
+	describe( 'editing pipeline conversion', () => {
+		it( 'should convert block with defined item', () => {
+			setModelData( doc, '<paragraph schemaItem="Article">[]foo</paragraph>' );
+
+			expect( editor.getData() ).to.equal( '<p itemtype="http://schema.org/Article" itemscope="itemscope">foo</p>' );
+		} );
 	} );
 } );
